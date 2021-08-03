e1be12782ca37245ec26894a860cb54b0af67bb5
Sasan Sotoodehfar <sasan1200@gmail.com>
2021-08-02 20:18:39 -0400

    testersChoiceApi

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,7 @@ import Login from '../views/Login.vue'
 import Logout from '../views/Logout.vue'
 import Register from '../views/Register.vue'
 import store from '../store/index'
+import HereMap from '../components/HereMap.vue'
 
 Vue.use(Router)
 
@@ -53,6 +54,14 @@ const router = new Router({
         requiresAuth: false
       }
     },
+    {
+      path: "/map",
+      name: "map",
+      component: HereMap,
+      meta: {
+        requiresAuth: false
+      }
+    }
   ]
 })
 
