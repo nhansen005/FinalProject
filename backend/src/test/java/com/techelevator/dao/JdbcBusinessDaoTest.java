34f38a8c5a18057aca105b255c631bc273c0d439
Deji Akinbode <Ayodejiakinbode19@gmail.com>
2021-08-11 07:18:27 -0400

    t

---
+++ backend/src/test/java/com/techelevator/dao/JdbcBusinessDaoTest.java
@@ -0,0 +1,10 @@
+package com.techelevator.dao;
+import com.techelevator.model.Business;
+import com.techelevator.model.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class JdbcBusinessDaoTest extends FinalCapstoneDaoTests{
+}
